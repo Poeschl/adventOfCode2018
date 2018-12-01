ac989386e23ff670bba779ba6dcfdcd0abe1b91e
Markus Pöschl <Poeschl@users.noreply.github.com>
2018-12-01 17:25:55 +0100

    Day 1 answer 2

--- src/main/kotlin/io/github/poeschl/adventofcode2018/day1/ChonalClibration.kt
+++ src/main/kotlin/io/github/poeschl/adventofcode2018/day1/ChonalClibration.kt
@@ -6,8 +6,12 @@ fun main(args: Array<String>) {
     val input = File(ClassLoader.getSystemResource("day1/input.txt").file)
     val startFrequency = 0
 
-    val result = calcFrequency(input, startFrequency)
-    println(result)
+    val result1 = calcFrequency(input, startFrequency)
+    println("First Part: $result1")
+
+
+    val result2 = findDoupledFrequency(input, startFrequency)
+    println("Second Part: $result2")
 }
 
 fun calcFrequency(inputFile: File, startFrequency: Int): Int {
@@ -17,4 +21,25 @@ fun calcFrequency(inputFile: File, startFrequency: Int): Int {
         frequency += it.toInt()
     }
     return frequency;
+}
+
+fun findDoupledFrequency(inputFile: File, startFrequency: Int): Int {
+    var frequency = startFrequency
+    val visitedFrequencies = mutableListOf<Int>()
+    var doubleFreq: Int? = null
+
+    do {
+        for (line: String in inputFile.readLines()) {
+            frequency += line.toInt()
+
+            if (visitedFrequencies.contains(frequency)) {
+                doubleFreq = frequency
+                break
+            } else {
+                visitedFrequencies.add(frequency)
+            }
+        }
+    } while (doubleFreq == null)
+
+    return doubleFreq
 }
